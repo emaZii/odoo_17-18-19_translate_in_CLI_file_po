626ddf7306680479da3345f8f9dc13e70b846961
EmanueleZii <241270998+emaZii@users.noreply.github.com>
2026-02-27 14:56:07 +0100

    Update translate_module.py

--- translate_module.py
+++ translate_module.py
@@ -16,6 +16,7 @@ args = parser.parse_args()
 addons_path = os.path.abspath(args.path)
 
 odoo.tools.config['--addons-path'] = addons_path
+odoo.tools.config['db_host'] = '127.0.0.1'
 odoo.tools.config['--db_password'] = args.db_password
 odoo.tools.config['--db_port'] = args.db_port
 odoo.tools.config['--db_user'] = args.db_user
