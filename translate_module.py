4f47e53c684870ba8a026aa3a9fa4177f66a362f
EmanueleZii <241270998+emaZii@users.noreply.github.com>
2026-03-03 15:53:43 +0100

    Update translate_module.py

--- translate_module.py
+++ translate_module.py
@@ -15,36 +15,32 @@ args = parser.parse_args()
 
 addons_path = os.path.abspath(args.path)
 
-odoo.tools.config['addons-path'] = addons_path
+odoo.tools.config['addons_path'] = addons_path  # underscore!
 odoo.tools.config['db_host'] = '127.0.0.1'
 odoo.tools.config['db_password'] = args.db_password
-odoo.tools.config['db_port'] = args.db_port
+odoo.tools.config['db_port'] = int(args.db_port)  # int!
 odoo.tools.config['db_user'] = args.db_user
-    
 
 registry = odoo.modules.registry.Registry.new(args.db)
-    
+
 i18n_path = os.path.join(addons_path, args.module, 'i18n')
-    
+
 if not os.path.exists(i18n_path):
     os.makedirs(i18n_path, exist_ok=True)
     print(f"Cartella creata: {i18n_path}")
 
 if args.lang == 'en_US':
-    filename = os.path.join(i18n_path, f"{args.module}.po")
-    export_lang = False
-    export_format = 'po'
-elif args.lang == 'it_IT':
-    filename = os.path.join(i18n_path, f"{args.module}.po")
-    export_format = 'po'
+    filename = os.path.join(i18n_path, f"{args.module}.pot")
+    export_lang = False       
+    export_format = 'pot'
 else:
     filename = os.path.join(i18n_path, f"{args.lang}.po")
-    export_lang = args.lang
+    export_lang = args.lang 
     export_format = 'po'
-    
+
 with registry.cursor() as cr:
     with open(filename, 'wb') as f:
         trans_export(export_lang, [args.module], f, export_format, cr)
         print(f"File creato con successo: {filename}")
-            
-#registry = odoo.modules.registry.Registry.delete(args.db)
+
+#odoo.modules.registry.Registry.delete(args.db)
